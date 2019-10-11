d1908400ef79bd157b81f4730ec0ac90c5e835be
Ryan W <rpw003@mcdaniel.edu>
2019-10-11 00:25:32 -0400

    Made changes to both CSS and Javascript files.
    Created if statement that checks user input of principal balance, will convert input to an integer if needed. Made a few adjustments to CSS file, changed the colors of some UI components. Webpage is up and running.

--- mortgagecalculator.js
+++ mortgagecalculator.js
@@ -1,23 +1,25 @@
 // A function to calculate the mortgage
 function calcMortgage() {
-    var principal = document.getElementById ("principal").value;
+    let principal = document.getElementById ("principal").value;
 
-    let interest = document.getElementById("annualRate").value;
+    let annualRate = document.getElementById("annualRate").value;
 
     let mortgagePeriod = document.getElementById("mortgagePeriod").value;
 
-    // Validates the user's input
+    /* Validates the user's input */
     if (principal === "" || annualRate === "" || mortgagePeriod === "")  {
         alert ("Please input your principal, interest rate, and mortgage period.");
         return;
     
     } // if
 
-    // Calculates the mortgage
-    let annualRate = 4.32;
-    let principal = 265000;
-    let mortgagePeriod = 15;
+    /* Checks for non-numerical characters such as commas in the principal
+    user input */
+    if (typeof principal === 'string') {
+        principal = parseInt (principal.replace(/\D/g, ""));
     
+    } // if
+
     annualRate /= 100;
   
     let monthlyRate = (annualRate / 12);
@@ -33,14 +35,10 @@ function calcMortgage() {
   
     result = parseFloat (result.toFixed (4));
   
-    total = monthlyRate / result;
+    total = (monthlyRate / result) * principal;
   
-    total *= principal;
+    total = parseFloat (total.toFixed ());
   
-    total = total.toFixed ();
-  
-    total = parseFloat (total);
-    
     // Displays the monthly payment to the HTML document
     document.getElementById ("totalMortgage").style.display = "block";
     document.getElementById ("mortgage").innerHTML = total;
@@ -49,7 +47,7 @@ function calcMortgage() {
 
 // Hides the  amount upon loading 
 document.getElementById ("totalMortgage").style.display = "none";
-document.getElementById ("each").style.display = "none";
+document.getElementById ("monthly").style.display = "none";
 
 /* When a user clicks the 'Calculate' button, the calculateTip function
 is called */
